58347c312f17e14f418000f362664f8a8274e999
Tonstory <auttapon.siriwaramas@gmail.com>
2020-03-17 23:08:15 +0700

    test

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-console.log("Test")
-console.log("i'm new feature")
-console.log("i'm new feature3")
-console.log("Test4")
-console.log("i'm new feature")
+const fs = require('fs');
+let objMaps = JSON.parse(fs.readFileSync("D:/app/100002New.geojson"));
+let newLines =  objMaps.features
+                        .filter(objMap=>objMap.geometry.type != "Point")
+                        .map(objLine=>JSON.stringify(objLine))
+                        .join("\n");
+fs.writeFileSync("./line.geojson",newLines)
