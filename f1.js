9661bc5dec0f1ef35b56fb8587dc8461041a2174
Tonstory <auttapon.siriwaramas@gmail.com>
2020-09-14 23:16:30 +0700

    Revert "add another +++++"
    This reverts commit dafc08cd1b9cec0a17b68b21a9d6177b38617339.

--- f1.js
+++ f1.js
@@ -2,6 +2,6 @@ console.info("F1")
 console.log("F11-----------")
 console.info("f111")
 console.log("F11+++++++++++++")
-console.info("f111++++++++++++++++++++")
+console.info("f111")
 console.info("f222")
 
